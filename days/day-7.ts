8e687fbf78891d191630e882ab39b55cd8a45558
Gaston Lhoest <gaston.lhoest@gmail.com>
2024-12-08 18:43:42 +0100

    prep

---
+++ days/day-7.ts
@@ -0,0 +1,18 @@
+import readFileIntoArray from "../utils/readFile.ts";
+
+const input = (await readFileIntoArray(`./files/${__filename.split('\\').at(-1).replace('.ts', '')}`)).map(line => [...line])
+
+function part1(): string {
+    return ''
+}
+
+function part2(): string {
+    return ''
+}
+
+const fn = () => {
+    console.log('part 1: ', part1());
+    console.log('part 2: ', part2());
+}
+
+export default fn
